4970763f6182c6ef262a969300782b5ea58f1734
wizardkeven <wizardkeven@live.com>
2018-03-29 07:35:42 +0900

    changed flow

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -88,21 +88,24 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	//   implement this method and use it as a helper during the updateWeights phase.
 	// cout<<"Start dataAssociation!!\n";
 
-	for(int k = 0; k < observations.size(); k++){
+	for(int k = 0; k < observations.size(); k++)
+	{
 		//assign initial distance to the map range
-		double nearest_dist =  1.7*pow(10, 308);
+		double nearest_dist =  numeric_limits<double>::max();
 		int nearest_partical_id = -1;
 		//go over all landmarks on the map to find the nearest and assign the id
-		for(int n = 0; n< predicted.size(); n++){
+		for(int n = 0; n< predicted.size(); n++)
+		{
 			double dist_n = dist(observations[k].x, observations[k].y, predicted[n].x, predicted[n].y);
 			//if current observed landmark is the nearest then update
-			if(dist_n < nearest_dist){
+			if(dist_n < nearest_dist)
+			{
 				nearest_dist = dist_n;
 				nearest_partical_id = predicted[n].id;
 			}
 		}
 		//assign the current nearest landmark id on the map to the current observation
-		if(nearest_partical_id >= 0){
+		if(nearest_partical_id > 0){
 			observations[k].id = nearest_partical_id;
 			// cout<<"nearest dist:\t"<<nearest_dist<<"\twith id:\t"<<nearest_partical_id<<"\n";
 			// cout<<"\tobs_id:\t"<<observations[k].id<<"\tobs_x:\t"<<observations[k].x<<"\tobs_y:\t"<<observations[k].y<<"\n";
@@ -126,20 +129,25 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   http://planning.cs.uiuc.edu/node99.html
 
 	// cout<<"==========start to update==============\n";
-	weights.clear();
+	// weights.clear();
 	// cout<<"finished assign predicted vector!\n";
 
-	for(int i = 0 ; i < particles.size(); i++){
+	for(int i = 0 ; i < particles.size(); i++)
+	{
 
 			// get landmarks from map
 			vector<LandmarkObs> predicted;
-			for(int i = 0;i < map_landmarks.landmark_list.size(); i++){
-				double x_0 = map_landmarks.landmark_list[i].x_f;
-				double y_0 = map_landmarks.landmark_list[i].y_f;
+			Map::single_landmark_s slandmark;
+			for(int i = 0;i < map_landmarks.landmark_list.size(); i++)
+			{
+				slandmark = map_landmarks.landmark_list[i];
+				double x_0 = slandmark.x_f;
+				double y_0 = slandmark.y_f;
 				double dist_to_particle = dist(x_0, y_0, particles[i].x, particles[i].y);
-				if(dist_to_particle < sensor_range){
+				if(dist_to_particle < sensor_range)
+				{
 					LandmarkObs lm_i;
-					lm_i.id = map_landmarks.landmark_list[i].id_i;
+					lm_i.id = slandmark.id_i;
 					lm_i.x = x_0;
 					lm_i.y = y_0;
 					predicted.push_back(lm_i);
@@ -148,9 +156,10 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
 		vector<LandmarkObs> obs_global;
 		//go through all landmark observations and transform coordinate-axis to global map axis
-		for(int j = 0; j<observations.size();j++){
+		for(int j = 0; j<observations.size();j++)
+		{
 			LandmarkObs lm_global;
-			lm_global.id = observations[j].id;
+			lm_global.id = -1;
 			lm_global.x = particles[i].x + cos(particles[i].theta) * observations[j].x - sin(particles[i].theta) * observations[j].y;
 			lm_global.x = particles[i].y + (sin(particles[i].theta) * observations[j].x) + (cos(particles[i].theta) * observations[j].y);
 			obs_global.push_back(lm_global);
@@ -161,75 +170,121 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 		dataAssociation(predicted, obs_global);
 		// cout<<"finished dataAssociation\n";
 
-		// // set associations process
-		// std::vector<int> ids;
-		// std::vector<double> xs;
-		// std::vector<double> ys;
-		// for(int k =0; k<obs_global.size(); ++k){
-		// 	ids.push_back(obs_global[k].id);
-		// 	xs.push_back(obs_global[k].x);
-		// 	ys.push_back(obs_global[k].y);
-		// }
-		// cout<<"start SetAssociations!!\n";
-		// // SetAssociations(particles[i], ids, xs, ys);
-		// cout<<"finished SetAssociations!!\n";
-
 		std::vector<int> ids;
 		std::vector<double> xs;
 		std::vector<double> ys;
 
 		particles[i].weight = 1.0;
-		cout<<"starting weight:"<<particles[i].weight<<"\n";
+		weights[i] = 1.0;
+		// cout<<"starting weight:"<<particles[i].weight<<"\n";
 		//update weights for current particle using the result from previous association
-		double gauss_norm= (1/(2 * M_PI * std_landmark[0] * std_landmark[1]));
+		double gauss_norm = 1/(2 * M_PI * std_landmark[0] * std_landmark[1]);
+		double delta_x_2 = std_landmark[0]*std_landmark[0];
+		double delta_y_2 = std_landmark[1]*std_landmark[1];
 		// cout<<"gauss_norm: "<<gauss_norm<<"\tstd_x:\t"<<std_landmark[0]<<"\tstd_y:\t"<<std_landmark[1]<<"\n";
 		// calculate exponent
-		for(int n = 0; n<obs_global.size(); n++){
+		for(int n = 0; n<obs_global.size(); n++)
+		{
 
 			int id_m = obs_global[n].id;
 			// cout<<"calculating for id: "<<id_m<<"\tid: "<<obs_global[n].id<<"\tx: "<<obs_global[n].x<<"\ty: "<<obs_global[n].y<<"\n";
-			if(id_m >= 0){
+			for(int m = 0; m < predicted.size(); m++)
+			{
+				if(id_m == predicted[m].id)
+				{
+					double diff_x = obs_global[n].x - predicted[id_m-1].x;
+					double diff_y = obs_global[n].y - predicted[id_m-1].y;
+					double exponent= diff_x*diff_x/(2*delta_x_2) + diff_y*diff_y/(2*delta_y_2);
+					double wk = gauss_norm * exp(-exponent);
+					if(wk > 0.0){
+						particles[i].weight *=wk;
+					}
+					// break;
 
-				double diff_x = obs_global[n].x - predicted[id_m].x;
-				double diff_y = obs_global[n].y - predicted[id_m].y;
-
-				if(diff_x > sensor_range || diff_y > sensor_range){
-					continue;
+					// if(diff_x > sensor_range || diff_y > sensor_range){
+					// 	continue;
+					// }
+					// cout<<id_m<<"-th accumulated weight:\t"<<wk<<"\n";
+					// if(wk > 0.0)
+					// {
+					
+					// }
 				}
-				double exponent= diff_x*diff_x/(2*std_landmark[0]*std_landmark[0]) + diff_y*diff_y/(2*std_landmark[1]*std_landmark[1]);
-
-				double wk = gauss_norm * exp(-exponent);
-				// cout<<id_m<<"-th accumulated weight:\t"<<wk<<"\n";
-				if(wk > 0.0){
-					ids.push_back(id_m+1);
-					xs.push_back(predicted[id_m].x);
-					ys.push_back(predicted[id_m].y);
-
-					particles[i].weight *=wk;
-				}
-				// cout<<id_m<<"-th diff_x:\t"<<diff_x<<"\tdiff_y:\t"<<diff_y<<"\texponent part:\t"<<exponent<<"\texponent\t"<<exp(-exponent)<<"\tpar_w:\t"<<particles[i].weight<<"\twk:\t"<<wk<<"\n";
 			}
-			// cout<<id_m<<"-th accumulated weight:\t"<<particles[i].weight <<"\n";
+
+
+			ids.push_back(id_m);
+			xs.push_back(obs_global[n].x);
+			ys.push_back(obs_global[n].y);
+
 		}
-		if(particles[i].weight == 1.0){
-			particles[i].weight = 0.0;
-		}else{
+
+		if(particles[i].weight >= 1.0)
+		{
+			particles[i].weight == 0.0;
+		}
+		// else
+		// {
+		weights[i] = particles[i].weight;
+		// }
+		// weights[i] = particles[i].weight;
+			// if(id_m == predicted[])
+			// {
+			// 	double diff_x = obs_global[n].x - predicted[id_m-1].x;
+			// 	double diff_y = obs_global[n].y - predicted[id_m-1].y;
+
+			// 	// if(diff_x > sensor_range || diff_y > sensor_range){
+			// 	// 	continue;
+			// 	// }
+			// 	double exponent= diff_x*diff_x/(2*delta_x_2) + diff_y*diff_y/(2*delta_y_2);
+
+			// 	double wk = gauss_norm * exp(-exponent);
+			// 	// cout<<id_m<<"-th accumulated weight:\t"<<wk<<"\n";
+			// 	if(wk > 0.0)
+			// 	{
+			// 		ids.push_back(id_m);
+			// 		xs.push_back(obs_global[n].x);
+			// 		ys.push_back(obs_global[n].y);
+
+			// 		particles[i].weight *=wk;
+			// 	}
+			// 	// cout<<id_m<<"-th diff_x:\t"<<diff_x<<"\tdiff_y:\t"<<diff_y<<"\texponent part:\t"<<exponent<<"\texponent\t"<<exp(-exponent)<<"\tpar_w:\t"<<particles[i].weight<<"\twk:\t"<<wk<<"\n";
+			// }
+			// cout<<id_m<<"-th accumulated weight:\t"<<particles[i].weight <<"\n";
+		// }
+
+		// if(particles[i].weight == 1.0)
+		// {
+		// 	particles[i].weight = 0.0;
+		// }
+		
+		// if(particles[i].weight > 0.0)
+		// {
+			// cout<<"starting calculate weight for :"<<particles[i].id<<endl;
 			SetAssociations(particles[i], ids, xs, ys);
 			// cout<<"finished calculate weight for :"<<particles[i].id<<endl;
-			weights.push_back(particles[i].weight);
+		// }
+		// weights.push_back(particles[i].weight);
 			cout<<"Weight of "<<i<<"-th particle: "<<particles[i].weight<<"\n";
-		}
 		// calculate normalization term
 
 	}
 
 	//normalize weights for particles
-	double weights_sum = std::accumulate(weights.begin(), weights.end(), 0);
-	for(int i = 0; i<particles.size(); i++){
-		particles[i].weight /= weights_sum;
-		weights[i] = particles[i].weight;
-		cout<<i<<"-th weight: "<<particles[i].weight<<"\n";
-	}
+	// double weights_sum = 0;
+	// for(int k = 0; k < particles.size();k++)
+	// {
+	// 	weights_sum += particles[k].weight;
+	// }
+	
+	// cout<<"Total weights: "<<weights_sum<<"\n";
+	// // weights.clear();
+	// for(int i = 0; i<particles.size(); i++)
+	// {
+	// 	particles[i].weight /= weights_sum;
+	// 	weights[i] = particles[i].weight;
+	// 	cout<<i<<"-th weight: "<<particles[i].weight<<"\n";
+	// }
 }
 
 void ParticleFilter::resample() {
@@ -238,7 +293,8 @@ void ParticleFilter::resample() {
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
 	// cout<<"Start resample!\n";
 	random_device rd;
-	mt19937 gen(rd());
+	// mt19937 gen(rd());
+	default_random_engine gen;
 
 	double max_w = 0;
 	for( int i=0; i< particles.size();i++){
@@ -247,31 +303,61 @@ void ParticleFilter::resample() {
 		}
 	}
 
-	// cout<<"got max weight: "<<max_w<<"\n";
+	std::vector<Particle> new_particles;
+	cout<<"got max weight: "<<max_w<<"\n";
+	// uniform random distribution
+	uniform_real_distribution<double> unirealdist(0.0, 1.0);
+
 	double beta = 0.0;
 	int p_size = particles.size();
-	int selected[p_size];
-	int init_id = rand()%particles.size();
+	int init_id = rand() % p_size;
+
 	discrete_distribution<> d(weights.begin(), weights.end());
-	int pnum = particles.size();
-	for(int i = init_id; i< pnum+init_id;i++){
-		beta += d(gen)/max_w;
-		while(particles[i].weight < beta){
-			beta -= particles[i].weight;
-			// particles.erase(particles.begin()+i);
+	//Selected particles
+	// int selected[p_size];
+	// //initialize elements to 0 representing not selected
+	// for(int i =0 ; i< p_size;i++)
+	// {
+	// 	selected[i] = 0;
+	// }
+
+	for(int i = init_id; i< p_size+init_id;i++)
+	{
+		beta += unirealdist(gen) * 2.0 * max_w;
+		double wt = weights[i];
+		while ( wt < beta)
+		{
+			beta -= weights[i];
 			i++;
+			if(i >= p_size+init_id)
+				break;
+			wt = weights[i%p_size];
+			cout<<"Drop "<<i%p_size<<"-th particle with weight: "<<weights[i%p_size]<<"\n";
 		}
-		//else take this particle
-		selected[i] = 1;
+		new_particles.push_back(particles[i]);
+		// new_particles.push_back(particles[d(gen)]);
+		// // if(particles[i].weight > 0.0)
+		// {
+		// 	beta = beta + d(gen)/max_w;
+		// 	while(particles[i].weight < beta){
+		// 		beta = beta - particles[i].weight;
+		// 		// particles.erase(particles.begin()+i);
+		// 		i++;
+		// 	}
+		// 	//else take this particle
+		// 	Particle  new_particle = particles[i];
+		// 	new_particles.push_back(new_particle);
+		// }
 	}
-	
-	int deleted_count = 0;
-	for(int i = 0; i < p_size;i++){
-		if(selected[i] != 1){
-			particles.erase(particles.begin()+i-deleted_count);
-			deleted_count++;
-		}
-	}
+	particles = new_particles;
+	// for(int i = 0; i < p_size;i++)
+	// {
+	// 	if(selected[i] < 1)
+	// 	{//not selected
+	// 		particles.erase(particles.begin() + i - deleted_count);
+	// 		deleted_count++;
+	// 	}
+	// }
 }
 
 Particle ParticleFilter::SetAssociations(Particle& particle, const std::vector<int>& associations, 
@@ -281,9 +367,16 @@ Particle ParticleFilter::SetAssociations(Particle& particle, const std::vector<i
     // associations: The landmark id that goes along with each listed association
     // sense_x: the associations x mapping already converted to world coordinates
     // sense_y: the associations y mapping already converted to world coordinates
-    particle.associations= associations;
-    particle.sense_x = sense_x;
-    particle.sense_y = sense_y;
+	//Clear the previous associations
+	particle.associations.clear();
+	particle.sense_x.clear();
+	particle.sense_y.clear();
+
+	particle.associations= associations;
+ 	particle.sense_x = sense_x;
+ 	particle.sense_y = sense_y;
+
+ 	return particle;
 }
 
 string ParticleFilter::getAssociations(Particle best)
